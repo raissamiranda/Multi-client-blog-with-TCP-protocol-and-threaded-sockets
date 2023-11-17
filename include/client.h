fcf85565f5a4e339706586a79cd82b8062df5f94
raissamiranda <raissamm01@hotmail.com>
2023-11-16 21:11:43 -0300

    Creating client

--- include/client.h
+++ include/client.h
@@ -1,3 +1,19 @@
 #include "common.h"
 
 pthread_t clientThread;
+char command[2048];
+char *topic = " ";
+
+struct Command {
+    const char *name;
+    int returnValue;
+};
+
+
+struct Command commands[] = {
+    {"exit", DISCONNECT},
+    {"list topics", LIST_TOPICS},
+    {"subscribe", SUBSCRIBE_IN_TOPIC},
+    {"unsubscribe", UNSUBSCRIBE_IN_TOPIC},
+    {"publish", NEW_POST_IN_TOPIC}
+};
--- src/client.c
+++ src/client.c
@@ -38,9 +38,80 @@ int main(int argc, char **argv)
     // Create thread to handle the client
     pthread_create(&clientThread, NULL, waitingFunction, (void *)&sockfd);
 
+    while (1)
+    {
+
+        fgets(command, sizeof(command), stdin);
+        int commandType = handleCommand(command);
+
+        if (commandType == INVALID)
+        {
+            printf("Invalid command\n");
+            continue;
+        }
+
+        switch(commandType) {
+            case NEW_POST_IN_TOPIC:
+                break;
+            case LIST_TOPICS:
+                break;
+            case SUBSCRIBE_IN_TOPIC:
+                break;
+            case UNSUBSCRIBE_IN_TOPIC:
+                break;
+            case DISCONNECT:
+                break;
+            default:
+                break;
+        }
+    }
+
     return 0;
 }
 
 void waitingFunction()
 {
+}
+
+int handleCommand(char *input)
+{
+    input[strlen(input) - 1] = '\0';    // Remove \n from input
+    char *command = strtok(input, " "); // Get the command
+
+    for (int i = 0; i < sizeof(commands) / sizeof(struct Command); i++)
+    {
+        // Check if the command is valid
+        if (strncmp(input, commands[i].name, strlen(commands[i].name)) == 0)
+        {
+            if (strcmp(command, "exit") == 0)
+            {
+                return DISCONNECT;
+            }
+            if (strcmp(command, "list topics") == 0)
+            {
+                return LIST_TOPICS;
+            }
+            if (strcmp(command, "subscribe") == 0 || strcmp(command, "unsubscribe") == 0)
+            {
+                char *inKeyword = strtok(NULL, " ");
+                if (inKeyword == NULL || !(strcmp(inKeyword, "in") == 0 || strcmp(inKeyword, "to") == 0))
+                {
+                    return INVALID;
+                }
+                return (strcmp(command, "subscribe") == 0) ? SUBSCRIBE_IN_TOPIC : UNSUBSCRIBE_IN_TOPIC;
+            }
+            if (strcmp(command, "publish") == 0)
+            {
+                char *inKeyword = strtok(NULL, " ");
+                if (inKeyword == NULL || strcmp(inKeyword, "in") != 0)
+                {
+                    return INVALID;
+                }
+                return NEW_POST_IN_TOPIC;
+            }
+            return INVALID;
+        }
+
+        return INVALID;
+    }
 }
